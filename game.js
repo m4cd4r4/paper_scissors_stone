6c3d7d57778d8fe510d26da47259f493f8418f35
m4cd4r4 <m4cd4r4@gmail.com>
2022-05-13 20:52:59 +0800

    Change 'if-else' statements from console.log to return

--- game.js
+++ game.js
@@ -1,28 +1,36 @@
 const computerOptions = ["rock", "paper", "scissors"];
 
+// Automate computer's choice with a random selection from the 3 options above
+
 function computerPlay() {
   let randomChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
   console.log('Computer choice is', randomChoice);
   return randomChoice;
 }
 
+// Play a single round with the computer
 
 function playRound(playerSelection, computerSelection) {
   
   playerSelection = prompt("Please input rock, paper or scissors:").toLowerCase();
-  console.log('playSelect is', playerSelection);
+  // console.log('playSelect is', playerSelection);
 
   computerSelection = computerPlay();
   console.log('compPlay is',computerSelection);
 
   if (playerSelection === computerSelection) {
-    console.log('The game is a tie!');
+    return('The game is a tie!');
   }    
   else if ((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'paper' && computerSelection === 'scissors')) {
-    console.log('Computer wins!');
+    return('Computer wins!');
   }
   else {
-    console.log('Player wins!');
+    return('Player wins!');
   }
 
 }
+
+// Test the playRound function before continuing to multiple round function
+console.log(playRound());
+
+

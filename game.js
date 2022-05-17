0fc1ebfbaa577321a4d902803d9dab4d6aa8fb39
m4cd4r4 <m4cd4r4@gmail.com>
2022-05-17 15:47:23 +0800

    Add "Best out of Five" function

--- game.js
+++ game.js
@@ -1,36 +1,58 @@
 const computerOptions = ["rock", "paper", "scissors"];
+playerScore = 0;
+computerScore = 0;
 
-// Automate computer's choice with a random selection from the 3 options above
-
-function computerPlay() {
+// 1 - Automate computer's choice with a random selection from the 3 options above
+function computerPlay() { 
   let randomChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
   console.log('Computer choice is', randomChoice);
   return randomChoice;
 }
 
-// Play a single round with the computer
-
-function playRound(playerSelection, computerSelection) {
+// 2 - Play a single round with the computer
+function playRound(playerSelection, computerSelection) { 
   
   playerSelection = prompt("Please input rock, paper or scissors:").toLowerCase();
-  // console.log('playSelect is', playerSelection);
-
   computerSelection = computerPlay();
   console.log('compPlay is',computerSelection);
 
   if (playerSelection === computerSelection) {
     return('The game is a tie!');
   }    
-  else if ((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'paper' && computerSelection === 'scissors')) {
+  else if ((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'paper' && computerSelection === 'scissors')) 
+  {
+ 
     return('Computer wins!');
   }
   else {
+    playerScore += 1;
     return('Player wins!');
   }
-
 }
 
-// Test the playRound function before continuing to multiple round function
-console.log(playRound());
+// 3 - Test the playRound function before continuing to multiple round function
+console.log(playRound()); 
+
+// 4 - Play a 5-round game
+function game() {  
+  
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    }
+  if (playerScore > computerScore) {
+    console.log("The player wins!")
+  } 
+  else if (computerScore > playerScore) {
+    console.log("The computer wins!")
+  }
+}
+
+game();
+
+
+
 
 
